b485bd5224c64ee4eeb6cd396c4e625ac51fc865
ZipheR_Gitmoh <zamangwane7@live.com>
2022-11-22 22:34:12 +0200

    Sentence Class & Model created

--- Sentence_Builder/Controllers/HomeController.cs
+++ Sentence_Builder/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Sentence_Builder.Models;
 using System.Diagnostics;
+using Sentence_Builder_Backend;
+//using Sentence_Builder.Models;
 
 namespace Sentence_Builder.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public Sentence SentenceBL = new Sentence();
+        public string sentence = string.Empty;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -27,6 +31,24 @@ namespace Sentence_Builder.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [HttpGet]
+        public IActionResult GetWordList(int wordType)
+        {
+            List<string> wordTypes = SentenceBL.GetWordsByType(wordType);
+
+            return View();
+        }
+
+        public string AddWordToSentence(string word) => sentence = sentence + " " + word;
+
+        public IActionResult OnSubmitSentence(string sentence)
+        {
+            List<string> sentences = SentenceBL.SaveSentence(sentence);
+
+            return View();
         }
     }
 }
---
+++ Sentence_Builder/Models/SentenceModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Sentence_Builder.Models
+{
+    public class SentenceModel
+    {
+        public List<string> sentences = new List<string>();
+
+        [Required]
+        [Display(Name = "Word Type")]
+        public int WordType { get; set; }
+        public IEnumerable<SelectListItem>? WordTypes { get; set; }
+
+        [Display(Name = "Word List")]
+        public string? Word { get; set; }
+        public IEnumerable<SelectListItem>? Words { get; set; }
+    }
+}
